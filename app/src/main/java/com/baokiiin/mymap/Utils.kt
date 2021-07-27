7ec8dfceb2c655c8185dc49abe05dc6ae243e35b
DESKTOP-TTLUSJ2\baoki <baokiinkk@gmail.com>
2021-07-27 10:21:37 +0700

    feat(showinfo): show dialog bottom sheet
    hiện thông tin và vẽ đường đi từ vị trí hiện tại đến market đó

--- app/src/main/java/com/baokiiin/mymap/Utils.kt
+++ app/src/main/java/com/baokiiin/mymap/Utils.kt
@@ -1,9 +1,17 @@
 package com.baokiiin.mymap
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.textfield.TextInputLayout
 
 object  Utils {
 
@@ -11,6 +19,25 @@ object  Utils {
     fun showDialog(fragmentActivity: FragmentActivity) {
         val diaLogFragment = ListLocationFragment()
         diaLogFragment.show(fragmentActivity.supportFragmentManager,TAG)
+    }
+    fun diaLogBottom(
+        context: Context,
+        layoutInflater: LayoutInflater,
+        market: Market,
+        action:()->Unit
+    ): BottomSheetDialog {
+        val sheetDialog = BottomSheetDialog(context, R.style.SheetDialog)
+        val viewDialog = layoutInflater.inflate(R.layout.user_dialog, null)
+        viewDialog.apply {
+            findViewById<TextView>(R.id.txtTitleMarket).text = market.title
+            findViewById<TextView>(R.id.txtDescription).text = market.description
+            findViewById<ExtendedFloatingActionButton>(R.id.btnDirection).setOnClickListener {
+                action()
+                sheetDialog.dismiss()
+            }
+        }
+        sheetDialog.setContentView(viewDialog)
+        return sheetDialog
     }
 
 }
